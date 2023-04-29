cc65d57a35be60f9ca56848fc5abd10de5e45d3b
Abby <abyie16@gmail.com>
2023-04-29 23:47:56 +0200

    Contrasenia olvidada funcionando

---
+++ app/src/main/java/com/example/epubook/controlador/ControlUsuario.java
@@ -0,0 +1,6 @@
+package com.example.epubook.controlador;
+
+
+public class ControlUsuario {
+
+}
--- app/src/main/java/com/example/epubook/vista/InicioSesion.java
+++ app/src/main/java/com/example/epubook/vista/InicioSesion.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Patterns;
@@ -15,6 +16,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.epubook.R;
+import com.example.epubook.controlador.ControlUsuario;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -39,6 +42,8 @@ public class InicioSesion extends AppCompatActivity {
     private TextView iniRegistro;
     private TextView olv_Contrasenia;
 
+    private ControlUsuario controlUsuario = new ControlUsuario();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,18 +56,20 @@ public class InicioSesion extends AppCompatActivity {
         iniRegistro = findViewById(R.id.btini_reg);
         olv_Contrasenia = findViewById(R.id.ini_olvCtr);
 
+        //Al pulsar botón de iniciar sesión.
         botonIni.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(!comprobarUsuario() | !comprobarContr()){
 
                 }else{
-                    
+
                     validarUsuario();
                 }
             }
         });
 
+        //Al pulsar text registrarse.
         iniRegistro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -72,9 +79,11 @@ public class InicioSesion extends AppCompatActivity {
             }
         });
 
-       /* olv_Contrasenia.setOnClickListener(new View.OnClickListener() {
+        //Al pulsar text contrasenia olvidada.
+        olv_Contrasenia.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 AlertDialog.Builder builder = new AlertDialog.Builder(InicioSesion.this);
                 View d_contrasenia = getLayoutInflater().inflate(R.layout.dialogo_contrasenia, null);
                 EditText email = d_contrasenia.findViewById(R.id.olv_email);
@@ -82,20 +91,49 @@ public class InicioSesion extends AppCompatActivity {
                 builder.setView(d_contrasenia);
                 AlertDialog alertDialog = builder.create();
 
+                //Al pulsar en el botón cambiar del dialog.
                 d_contrasenia.findViewById(R.id.btolv_cambiar).setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         String usuarioEmail = email.getText().toString();
 
+                        //Compruebo correo introducido.
                         if(TextUtils.isEmpty(usuarioEmail) && !Patterns.EMAIL_ADDRESS.matcher(usuarioEmail).matches()){
-                            Toast.makeText(InicioSesion.this, "Ingresa tu email", Toast.LENGTH_SHORT);
+                            Toast.makeText(InicioSesion.this, "Ingresa tu email", Toast.LENGTH_SHORT).show();
                             return;
                         }
 
+                        //Firebase auth: método para reestablecer contrasenia.
+                        auth.sendPasswordResetEmail(usuarioEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(InicioSesion.this, "Comprueba tu bandeja de correos.", Toast.LENGTH_SHORT).show();
+                                    alertDialog.dismiss();
+                                }else {
+                                    Toast.makeText(InicioSesion.this, "Error, email incorrecto.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
                     }
                 });
+
+                //Al pulsar botón cancelar del dialog.
+                d_contrasenia.findViewById(R.id.btolv_cancelar).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        alertDialog.dismiss();
+                    }
+                });
+
+                if(alertDialog.getWindow() != null){
+                    alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+                }
+
+                alertDialog.show();
             }
-        });*/
+        });
 
     }
 
